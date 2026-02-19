d9d3eb56646f4520fef61bcd37e81cb13571f429
veeravenna <veeravenna17895356@gmail.com>
2026-02-19 14:45:45 +0530

    Create Main.java file

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub!");
+        System.out.println("This is a Java file created on GitHub");
+    }
+}
