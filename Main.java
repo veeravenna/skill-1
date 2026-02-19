f1b6bc4d729b778fa728ab8287c54a943d85c867
veeravenna <veeravenna17895356@gmail.com>
2026-02-19 14:47:51 +0530

    initial commit with maim.java and notes . txt

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main { 
     public static void main(String[] args) {
         System.out.println("Hello from GitHub!");
         System.out.println("This is a Java file created on GitHub");
